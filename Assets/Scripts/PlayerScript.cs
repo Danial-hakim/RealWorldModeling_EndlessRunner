0b263b7116279564839c62d4de5f32d95e3b3f20
Danial <danialhakim01@gmail.com>
2022-10-17 12:59:31 +0100

    Invincibility power up is done , player flash red n white if they are invincible

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 10.0f;
+    private float jumpingPower = 20.0f;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private bool isInvincible;
+    private float invincibleDuration = 2.5f;
+    private float invincibleDeltaTime = 0.3f;
+
+    public Renderer renderer;
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Obstacles"))
+        {
+            if(!isInvincible)
+            {
+                loseHealth();
+                Debug.Log("Hit");
+            }
+        }
+    }
+
+    private IEnumerator BecomeTemporarilyInvincible()
+    {
+        isInvincible = true;
+
+        for (float i = 0; i < invincibleDuration; i += invincibleDeltaTime)
+        {
+            // Alternate between 0 and 1 scale to simulate flashing
+            if (renderer.material.color == Color.red)
+            {
+               renderer.material.color = Color.white;
+            }
+            else
+            {
+                renderer.material.color = Color.red;
+            }
+            yield return new WaitForSeconds(invincibleDeltaTime);
+        }
+
+        renderer.material.color = Color.white;
+        isInvincible = false;
+    }
+
+    public void loseHealth()
+    {
+        StartCoroutine(BecomeTemporarilyInvincible());
+    }
+}
